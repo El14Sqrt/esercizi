ae10b6418357a9bf723c042716e9a7bf807af1d3
El14Sqrt <4r0.squarti@gmail.com>
2024-04-18 13:38:51 +0200

    esercizio 3.1 svolto

--- lezione2.py
+++ lezione2.py
@@ -45,4 +45,12 @@ filename_without_extension = filename.removesuffix(".txt")
 
 print(filename_without_extension)
 
-#
+#3-1. Names: Store the names of a few of your friends in a list called names. 
+#Print each person’s name by accessing each element in the list, one at a time.
+
+names: list = ["Nicolò", "Marla", "Simone"]
+
+print(names[0])
+print(names[1])
+print(names[2])
+
