d9ffddd0d32ed2c1a7ff69896ac33c6bb87d4bd9
El14Sqrt <4r0.squarti@gmail.com>
2024-04-19 12:57:00 +0200

    aggiornata lezione 2

--- lezione2.py
+++ lezione2.py
@@ -72,3 +72,26 @@ print(f"I would like to own a {car[1]} motorcycle.")
 print(f"I would like to own a {car[2]} motorcycle.")
 print(f"I would like to own a {car[3]} motorcycle.")
 
+#3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? 
+#Make a list that includes at least three people you’d like to invite to dinner. 
+#Then use your list to print a message to each person, inviting them to dinner.
+
+invited = ["Michael Schumacher", "Ayrton Senna", "Charles Leclerc"]
+
+print(f"Ciao {invited[0]}, sei invitato a cena domani sera!")
+print(f"Ciao {invited[1]}, sei invitato a cena domani sera!")
+print(f"Ciao {invited[2]}, sei invitato a cena domani sera!")
+
+#3-5. Changing Guest List: You just heard that one of your guests can’t make the dinner, 
+#so you need to send out a new set of invitations. 
+#You’ll have to think of someone else to invite.
+
+#• Start with your program from Exercise 3-4. Add a print() call at the end of your program, 
+#stating the name of the guest who can’t make it.
+
+#• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+
+#• Print a second set of invitation messages, one for each person who is still in your list.
+
+print("ciao")
+
---
+++ lezione4.py
@@ -0,0 +1,5 @@
+def subtract(x, y) ->int:
+    sub = x - y
+    return sub
+result = subtract(45, 20)
+print(result +1)
