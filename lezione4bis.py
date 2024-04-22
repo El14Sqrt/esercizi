bd64f6a3a9c0f3cc35afd3f80af3db8f14e9ddff
El14Sqrt <4r0.squarti@gmail.com>
2024-04-22 16:38:53 +0200

    es 2 lez 4bis

--- lezione4bis.py
+++ lezione4bis.py
@@ -13,4 +13,20 @@ def is_pal(x: int) -> bool:
         i += 1
     return True
 
-#data una stringa s che contiene parole e spazi, restituire una stringa
+#data una stringa s che contiene parole e spazi, restituire la lunghezza dell'ultima parola in s.
+#una parola è una sottostringa che contiene caratteri contugui diversi dallo spazio.
+
+s: str = "oggi è lunedì"
+
+#modo 1
+l: list[str] = s.split()
+return len(1[-1])
+
+#modo 2
+i: int = 0
+curr_len: int = 0
+while i < len(s):
+    if s[i] != " ":
+        curr_len += 1
+    else:
+        break
