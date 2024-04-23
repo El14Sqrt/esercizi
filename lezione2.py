cb6019ec09b51d501eedb50d2d37148267e80e6a
El14Sqrt <4r0.squarti@gmail.com>
2024-04-23 15:55:04 +0200

    3-5 svolto

--- lezione2.py
+++ lezione2.py
@@ -93,5 +93,26 @@ print(f"Ciao {invited[2]}, sei invitato a cena domani sera!")
 
 #• Print a second set of invitation messages, one for each person who is still in your list.
 
-print("ciao")
+print(f"{invited[1]} non può venire alla cena")
+
+invited[1] = ("Sebastian Vettel")
+print(invited)
+
+print(f"Ciao {invited[0]}, sei invitato a cena domani sera!")
+print(f"Ciao {invited[1]}, sei invitato a cena domani sera!")
+print(f"Ciao {invited[2]}, sei invitato a cena domani sera!")
+
+#3-6. More Guests: You just found a bigger dinner table, so now more space is available. `
+#Think of three more guests to invite to dinner.
+
+#• Start with your program from Exercise 3-4 or 3-5. 
+#Add a print() call to the end of your program, informing people that you found a bigger table.
+
+#• Use insert() to add one new guest to the beginning of your list.
+
+#• Use insert() to add one new guest to the middle of your list.
+
+#• Use append() to add one new guest to the end of your list.
+
+#• Print a new set of invitation messages, one for each person in your list.
 
