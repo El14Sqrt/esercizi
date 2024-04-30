9a6f4aabeafc987a58a69b35acdaacab64902404
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-04-30 15:58:07 +0000

    6-1, 6-2 lezione 2

--- lezione2.1.py
+++ lezione2.1.py
@@ -4,3 +4,37 @@
 #You should have keys such as first_name, last_name, age, and city. 
 #Print each piece of information stored in your dictionary.
 
+person = {
+    "first_name" : "Mario",
+    "last_name" : "Rossi",
+    "age" : 30,
+    "city" : "Roma"
+}
+print(person["first_name"])
+print(person["last_name"])
+print(person["age"])
+print(person["city"])
+
+
+
+#6-2. 
+#Favorite Numbers: Use a dictionary to store people’s favorite numbers. 
+#Think of five names, and use them as keys in your dictionary. 
+#Think of a favorite number for each person, and store each as a value in your dictionary. 
+#Print each person’s name and their favorite number. 
+#For even more fun, poll a few friends and get some actual data for your program.
+
+favourite_numbers = {
+    "Francesco" : 15,
+    "Tobia" : 32,
+    "Vittorio" : 42,
+    "Federico" : 28,
+    "Leonardo" : 23
+}
+print(favourite_numbers["Federico"])
+print(favourite_numbers["Leonardo"])
+print(favourite_numbers["Federico"])
+print(favourite_numbers["Tobia"])
+print(favourite_numbers["Vittorio"])
+
+#
