1edc8d2cbc2dbc290e587e9226f96584ad8d3426
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-01 21:28:58 +0000

    8-11

--- lezione4.py
+++ lezione4.py
@@ -169,4 +169,21 @@ print(sent_messages)
 
 
 
-#
+#8-11. 
+#Archived Messages: Start with your work from Exercise 8-10. 
+#Call the function send_messages() with a copy of the list of messages. 
+#After calling the function, print both of your lists to show that the original list has retained its messages.
+
+text_messages: list = ["Hi, how are you today?", "Good afternoon!", "Do you want to hang out?"]
+sent_messages = []
+
+def send_messages():
+    for message in text_messages.copy():
+        print(message)
+        sent_messages.append(message)
+
+send_messages() 
+
+
+print("original list: ", text_messages)
+print("sent", sent_messages)
