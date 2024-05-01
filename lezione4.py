1169acb17e4dae3398a658f78d76a01f7abf1224
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-01 21:24:47 +0000

    8-10

--- lezione4.py
+++ lezione4.py
@@ -146,4 +146,27 @@ show_messages()
 
 
 
+#8-10. 
+#Sending Messages: Start with a copy of your program from Exercise 8-9. 
+#Write a function called send_messages() that prints each text message 
+#and moves each message to a new list called sent_messages as it’s printed. 
+#After calling the function, print both of your lists to make sure the messages were moved correctly.
+
+text_messages: list = ["Hi, how are you today?", "Good afternoon!", "Do you want to hang out?"]
+sent_messages = []
+
+def send_messages():
+    for message in text_messages:
+        print(message)
+        sent_messages.append(message)
+
+send_messages() 
+
+
+print(text_messages)
+print(sent_messages)
+
+
+
+
 #
