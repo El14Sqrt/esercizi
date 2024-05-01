7c31fec732651dca1233c4b55933975ba69041f3
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-01 20:15:30 +0000

    5-11

--- lezione3.1.py
+++ lezione3.1.py
@@ -206,8 +206,7 @@ elif alien_color == "red":
 
 #• If the person is age 65 or older, print a message that the person is an elder.
 
-age = 5
-#age = int(input())
+age = int(input())
 
 if age < 2:
     print("That person is a baby")
@@ -328,4 +327,26 @@ for user in low_new_users:
 
 
 
-#
+#5-11. 
+#Ordinal Numbers: Ordinal numbers indicate their position in a list, such as 1st or 2nd. 
+#Most ordinal numbers end in th, except 1, 2, and 3.
+
+#• Store the numbers 1 through 9 in a list.
+
+#• Loop through the list.
+
+#• Use an if-elif-else chain inside the loop to print the proper ordinal ending for each number. 
+#Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.
+
+numbers = list(range(1, 10))
+
+for number in numbers:
+    if number == 1:
+        ordinal = "1st"
+    elif number == 2:
+        ordinal = "2nd"
+    elif number == 3:
+        ordinal = "3rd"
+    else:
+        ordinal = str(number) + "th"
+    print(ordinal)
