4319faba6f396c37d13ffef4e1d5e67c81b98f16
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-01 21:55:30 +0000

    8-13, 8-14

--- lezione4.py
+++ lezione4.py
@@ -211,4 +211,44 @@ make_sandwich("tomato", "olives")
 
 
 
-#
+#8-13. 
+#User Profile:  Build a profile of yourself by calling build_profile(), 
+#using your first and last names and three other key-value pairs that describe you. 
+#All the values must be passed to the function as parameters. 
+#The function then must return a string such as "Eric Crow, age 45, hair brown, weight 67"
+
+def build_profile(first_name, last_name, age, hair_color, height):
+    profile = first_name + " " + last_name + ", age " + str(age) + ", hair " + hair_color + ", height " + str(height)
+    return profile
+
+my_profile = build_profile("Elia", "Squarti", 20, "brown", 170)
+
+print("My Profile:", my_profile)
+
+
+
+
+#8-14. 
+#Cars: Write a function that stores information about a car in a dictionary. 
+#The function should always receive a manufacturer and a model name. 
+#It should then accept an arbitrary number of keyword arguments. 
+#Call the function with the required information and two other name-value pairs, such as a color or an optional feature. 
+#Your function should work for a call like this one: car = make_car('subaru', 'outback', color='blue', tow_package=True) 
+#Print the dictionary that’s returned to make sure all the information was stored correctly. 
+
+def make_car(manufacturer, model, **keyword_arguments):
+    car = {"manufacturer": manufacturer, "model": model}
+    car.update(keyword_arguments)
+    return car
+
+
+car = make_car('subaru', 'outback', color='blue', tow_package=True)
+
+print(car)
+
+
+
+#8-15. 
+#Printing Models: Put the functions for the example printing_models.py in a separate file called printing_functions.py. Write an import statement at the top of printing_models.py, and modify the file to use the imported functions.
+
+
