3a0e6d5e1da88371f555f2897c175e2635ba0c4e
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-01 11:39:54 +0000

    4-2,4-3,4-4,4-5

--- lezione3.0.py
+++ lezione3.0.py
@@ -24,3 +24,54 @@ print("I really love pizza ", favourite_pizza[2])
 
 
 
+#4-2. 
+#Animals: Think of at least three different animals that have a common characteristic. 
+#Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
+
+animals = ["bats", "birds", "butterflies"]
+
+for animal in animals:
+    print(animal)
+
+    #• Modify your program to print a statement about each animal, such as A dog would make a great pet.
+
+    print(animal, "got wings")
+
+    #• Add a line at the end of your program, stating what these animals have in common. 
+    #You could print a sentence, such as Any of these animals would make a great pet!
+
+    print("animals have developed different types of wings, such as ", animal)
+
+
+
+#4-3. 
+#Counting to Twenty: Use a for loop to print the numbers from 1 to 20, inclusive.
+
+for i in range(1, 21):
+    print(i)
+
+
+
+#4-4. 
+#One Million: Make a list of the numbers from one to one million, and then use a for loop to print the numbers. 
+#(If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.)
+
+numbers = list(range(1,1000001))
+"""
+for n in numbers:
+    print(n)
+"""
+
+
+
+#4-5. 
+#Summing a Million: Make a list of the numbers from one to one million, 
+#and then use min() and max() to make sure your list actually starts at one and ends at one million. 
+#Also, use the sum() function to see how quickly Python can add a million numbers.
+
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+
+
+
