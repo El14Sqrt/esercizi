c7d0d29ec766c2d55ba94bbb609d626e012c10c8
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-01 20:39:18 +0000

    8-4

--- lezione4.py
+++ lezione4.py
@@ -38,4 +38,18 @@ make_shirt(size="large", message="I like snakes")
 
 
 
+#8-4. 
+#Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message 
+#that reads I love Python. 
+#Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+
+def make_shirt(size="large", message="I love Python"):
+    print("You've ordered a", size, "with written on it:", message)
+make_shirt()
+make_shirt(size="medium")
+
+make_shirt(size="small", message="I'm learning Python")
+
+
+
 #
