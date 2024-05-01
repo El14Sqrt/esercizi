c1f9f99f2e90609ebd62260adb7152d68264de3d
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-01 15:38:32 +0000

    4-10,  4-11

--- lezione3.0.py
+++ lezione3.0.py
@@ -118,3 +118,65 @@ print(cubes)
 cubes = [b**3 for b in range(1, 11)]
 print(cubes)
 
+
+
+
+#4-10. 
+#Slices: Using one of the programs you wrote in this chapter, 
+#add several lines to the end of the program that do the following:
+
+#userò la lista dei multipli di 3
+multiples_of_3 = list(range(3, 31, 3))
+
+#• Print the message The first three items in the list are: . 
+#Then use a slice to print the first three items from that program’s list.
+
+print("The first three items in the list are: ", multiples_of_3[:3])
+
+#• Print the message Three items from the middle of the list are: . 
+#Then use a slice to print three items from the middle of the list.
+
+print("Three items from the middle of the list are: ", multiples_of_3[3:6])
+
+#• Print the message The last three items in the list are: . 
+#Then use a slice to print the last three items in the list.
+
+print("The last three items in the list are: ", multiples_of_3[-3:])
+
+
+
+
+#4-11. 
+#My Pizzas, Your Pizzas: Start with your program from Exercise 4-1. 
+#Make a copy of the list of pizzas, and call it friend_pizzas. Then, do the following:
+
+friend_pizzas: list = favourite_pizza.copy()
+
+#• Add a new pizza to the original list.
+
+favourite_pizza.append("diavola")
+
+#• Add a different pizza to the list friend_pizzas.
+
+friend_pizzas.append("vegetarana")
+
+#• Prove that you have two separate lists. 
+#Print the message My favorite pizzas are: , and then use a for loop to print the first list. 
+print("\n")
+
+
+print("My favorite pizzas are: \n")
+
+for pizza in favourite_pizza: 
+    print(pizza)
+
+#Print the message My friend’s favorite pizzas are: , and then use a for loop to print the second list. 
+#Make sure each new pizza is stored in the appropriate list.
+print("\n")
+
+print("My friend’s favorite pizzas are: \n")
+
+for pizza in friend_pizzas:
+    print(pizza)
+    
+
