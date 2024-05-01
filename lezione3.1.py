23933452ded6151622bfed1741ec3721f1ba11e6
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-01 17:13:24 +0000

    5-5

--- lezione3.1.py
+++ lezione3.1.py
@@ -137,3 +137,54 @@ else:
     points += 10
     print("You just earned 10 points!")
 
+
+
+#5-5. 
+#Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif-else chain.
+
+#• If the alien is green, print a message that the player earned 5 points.
+
+#• If the alien is yellow, print a message that the player earned 10 points.
+
+#• If the alien is red, print a message that the player earned 15 points.
+
+#• Write three versions of this program, making sure each message is printed for the appropriate color alien.
+
+alien_color = "green"
+points: int = 0
+
+if alien_color == "green":
+    points += 5
+    print("You just earned 5 points!")
+elif alien_color == "yellow":
+    points += 10
+    print("You just earned 10 points!")
+elif alien_color == "red":
+    points += 15
+    print("You just earned 15 points!")
+
+alien_color = "yellow"
+points: int = 0
+
+if alien_color == "green":
+    points += 5
+    print("You just earned 5 points!")
+elif alien_color == "yellow":
+    points += 10
+    print("You just earned 10 points!")
+elif alien_color == "red":
+    points += 15
+    print("You just earned 15 points!")
+
+alien_color = "red"
+points: int = 0
+
+if alien_color == "green":
+    points += 5
+    print("You just earned 5 points!")
+elif alien_color == "yellow":
+    points += 10
+    print("You just earned 10 points!")
+elif alien_color == "red":
+    points += 15
+    print("You just earned 15 points!")
