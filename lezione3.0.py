20e3a69fc4b8b5d0e0e78cbe9e26aaac0e44e114
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-01 11:03:04 +0000

    4-1 lez 3

--- lezione3.0.py
+++ lezione3.0.py
@@ -1,10 +1,26 @@
 #4-1. Pizzas: Think of at least three kinds of your favorite pizza. 
 #Store these pizza names in a list, and then use a for loop to print the name of each pizza.
 
+
+
+favourite_pizza = ["margherita", "boscaiola", "marinara"]
+
+for pizza in favourite_pizza:
+    print(pizza)
+
 #• Modify your for loop to print a sentence using the name of the pizza, instead of printing just the name of the pizza. 
 #For each pizza, you should have one line of output containing a simple statement like I like pepperoni pizza.
+
+for pizza in favourite_pizza:
+    print("my favourite pizza is: ", pizza)
 
 #• Add a line at the end of your program, outside the for loop, that states how much you like pizza. 
 #The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, 
 #such as I really love pizza!
+
+print("I really love pizza ", favourite_pizza[0])
+print("I really love pizza ", favourite_pizza[1])
+print("I really love pizza ", favourite_pizza[2])
+
+
 
