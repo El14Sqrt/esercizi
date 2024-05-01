2bcef9ceaba7ea3b23138df7ba65035213e53767
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-01 18:24:14 +0000

    5-10

--- lezione3.1.py
+++ lezione3.1.py
@@ -296,4 +296,36 @@ else:
 
 
 
+
+#5-10. 
+#Checking Usernames: Do the following to create a program 
+#that simulates how websites ensure that everyone has a unique username.
+
+#• Make a list of five or more usernames called current_users.
+
+current_users = ["Dad's_pc", "person10254", "new_guest", "kids", "work"]
+
+#• Make another list of five usernames called new_users. 
+#Make sure one or two of the new usernames are also in the current_users list.
+
+new_users = ["Son's_pc", "new_guest", "David", "person10254", "Mom's_pc"]
+
+#• Loop through the new_users list to see if each new username has already been used. 
+#If it has, print a message that the person will need to enter a new username. 
+#If a username has not been used, print a message saying that the username is available.
+
+#• Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted. 
+#(To do this, you’ll need to make a copy of current_users containing the lowercase versions of all existing users.)
+
+low_current_users = [users.lower() for users in current_users]
+low_new_users = [users.lower() for users in new_users]
+
+for user in low_new_users:
+    if user in low_current_users:
+        print("you need to enter a new username")
+    else:
+        print("the username is available")
+
+
+
 #
