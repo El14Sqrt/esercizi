dba700b90af12ce46bb1adf9f823db11993905ce
El14Sqrt <4r0.squarti@gmail.com>
2024-05-02 10:51:31 +0200

    aggiornato primo esercizio

--- lezione5.py
+++ lezione5.py
@@ -2,12 +2,12 @@
 
 #Scrivi una funzione che calcola la media di una lista di numeri e ritorna il valore arrotondato all'intero più vicino.
 
-def rounded_average(numbers: list[int]) -> int:
+def rounded_average(numbers: list[int]) -> float:
     somma = 0
     for n in numbers:
         somma += n
     media = somma / len(numbers)
-    print(media)
-
-rounded_average([2, 3, 5, 7, 10])
-
+    rounded_media = round(media)
+    return rounded_media
+print(rounded_average([100, 200, 300, 400]))
+print(rounded_average([1, 1, 2, 2]))
