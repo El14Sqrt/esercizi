124859d7b716e6d2756b1bbde45f7a63b2940769
El14Sqrt <4r0.squarti@gmail.com>
2024-05-02 11:23:38 +0200

    terzo esercizio svolto

--- lezione5.py
+++ lezione5.py
@@ -27,3 +27,19 @@ def check_access(username: str, password: ..., is_active: bool) -> str:
 
 print(check_access("admin", "12345", True))
 print(check_access("admin", "54321", False))
+
+
+#ESERCIZIO 3
+#Scrivi una funzione che verifica se una combinazione di condizioni (A, B, e C) è soddisfatta per procedere con un'operazione. 
+#L'operazione può procedere solo se la condizione A è vera o se entrambe le condizioni B e C sono vere. 
+#La funzione deve ritornare "Operazione permessa" oppure "Operazione negata" a seconda delle condizioni che sono soddisfatte.
+
+def check_combination(conditionA: bool, conditionB: bool, conditionC: bool) -> str:
+    if conditionA == True or conditionB == True and conditionC == True:
+        return "Operazione permessa"
+    else:
+        return "Operazione negata"
+    
+print(check_combination(True, False, True))
+print(check_combination(False, True, False))
+print(check_combination(False, True, True))
