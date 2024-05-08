c6468dbedc1a4e4aa10fe086d1d5157930b3aceb
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-08 14:29:51 +0000

    9-1

--- lezione5.1.py
+++ lezione5.1.py
@@ -14,12 +14,15 @@ class Restaurant:
         self.cuisine_type = cuisine_type
     
     def describe_restaurant(self):
-        print(f"Restaurant(name{self.restaurant_name}, cuisine{self.cuisine_type}")
+        print(f"Restaurant(name: {self.restaurant_name}, cuisine: {self.cuisine_type})")
     
     def open_restaurant(self):
         print(f"Il ristorante {self.restaurant_name} è aperto.")
 
 r1 = Restaurant(restaurant_name="La vecchia Roma", cuisine_type="romana")
+
+r1.describe_restaurant()
+r1.open_restaurant()
 
 #9-2. 
 #Three Restaurants: Start with your class from Exercise 9-1. 
@@ -28,6 +31,16 @@ r1 = Restaurant(restaurant_name="La vecchia Roma", cuisine_type="romana")
 r2 = Restaurant(restaurant_name="", cuisine_type="")
 r3 = Restaurant(restaurant_name="", cuisine_type="")
 r4 = Restaurant(restaurant_name="", cuisine_type="")
+
+#9-4. 
+#Number Served: Start with your program from Exercise 9-1. 
+#Add an attribute called number_served with a default value of 0. Create an instance called restaurant from this class. 
+#Print the number of customers the restaurant has served, and then change this value and print it again. 
+#Add a method called set_number_served() that lets you set the number of customers that have been served. 
+#Call this method with a new number and print the value again. 
+#Add a method called increment_number_served() that lets you increment the number of customers who’ve been served. 
+#Call this method with any number you like that could represent how many customers were served in, say, a day of business. 
+
 
 
 #9-3. 
@@ -64,4 +77,12 @@ user2 = User(first_name="Diego",
              age=6,
              email="diegoemail@gmail.com",
              cf="unkown")
+
+#9-5. 
+#Login Attempts: Add an attribute called login_attempts to your User class from Exercise 9-3. 
+#Write a method called increment_login_attempts() that increments the value of login_attempts by 1. 
+#Write another method called reset_login_attempts() that resets the value of login_attempts to 0. 
+#Make an instance of the User class and call increment_login_attempts() several times. 
+#Print the value of login_attempts to make sure it was incremented properly, and then call reset_login_attempts(). 
+#Print login_attempts again to make sure it was reset to 0.
 
