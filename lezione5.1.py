c40373c70f91a7912399a798474f48a88b90c8d6
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-08 14:20:56 +0000

    esercizi lezione 5

---
+++ lezione5.1.py
@@ -0,0 +1,67 @@
+#ESERCIZI LEZIONE 5
+
+#9-1. 
+#Restaurant: Make a class called Restaurant. The __init__() method for Restaurant should store two attributes: 
+#a restaurant_name and a cuisine_type. 
+#Make a method called describe_restaurant() that prints these two pieces of information, 
+#and a method called open_restaurant() that prints a message indicating that the restaurant is open. 
+#Make an instance called restaurant from your class. 
+#Print the two attributes individually, and then call both methods.
+
+class Restaurant:
+    def __init__(self, restaurant_name:str, cuisine_type:str):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+    
+    def describe_restaurant(self):
+        print(f"Restaurant(name{self.restaurant_name}, cuisine{self.cuisine_type}")
+    
+    def open_restaurant(self):
+        print(f"Il ristorante {self.restaurant_name} è aperto.")
+
+r1 = Restaurant(restaurant_name="La vecchia Roma", cuisine_type="romana")
+
+#9-2. 
+#Three Restaurants: Start with your class from Exercise 9-1. 
+#Create three different instances from the class, and call describe_restaurant() for each instance.
+
+r2 = Restaurant(restaurant_name="", cuisine_type="")
+r3 = Restaurant(restaurant_name="", cuisine_type="")
+r4 = Restaurant(restaurant_name="", cuisine_type="")
+
+
+#9-3. 
+#Users: Make a class called User. Create two attributes called first_name and last_name, 
+#and then create several other attributes that are typically stored in a user profile. 
+#Make a method called describe_user() that prints a summary of the user’s information. 
+#Make another method called greet_user() that prints a personalized greeting to the user. 
+#Create several instances representing different users, and call both methods for each user.
+
+class User:
+    def __init__(self,
+                 first_name:str,
+                 last_name:str,
+                 age:int,
+                 email:str,
+                 cf:str):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.cf=cf
+        self.age=age
+        self.email=email
+    
+    def __str__(self) -> str:
+        return f"L'utente corrente si chiama {self.first_name} {self.last_name}, ha {self.age} anni, il su CF è {self.cf} e la sua mail è {self.email}"
+    
+user1 = User(first_name="Elia", 
+             age=19,
+             last_name="Squarti",
+             email="myemail@gmail.com",
+             cf="not_defined")
+
+user2 = User(first_name="Diego",
+             last_name="Pica",
+             age=6,
+             email="diegoemail@gmail.com",
+             cf="unkown")
+
