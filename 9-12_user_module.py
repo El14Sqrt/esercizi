e336d00e7de9a2a151172eefd56e10209a71c340
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-12 21:40:41 +0000

    9-12 prove file

---
+++ 9-12_user_module.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, first_name: str, last_name: str, age: int, email: str, cf: str):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.email = email
+        self.cf = cf
