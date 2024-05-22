26dd7bbfc6406144216bd24c22afd2b876eb1ec7
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-22 16:15:19 +0000

    es 7

--- lezione10/lezione10.py
+++ lezione10/lezione10.py
@@ -124,8 +124,34 @@ print("lunghezza ipotenusa: ", hypotenuse(8.0, 15.0)) # --> 17.0
 #Scrivi una funzione che rimuove tutti i duplicati da una lista, contenente sia numeri che lettere, 
 #mantenendo l'ordine originale degli elementi.
 
-def remove_duplicates(...) -> list:
-    
+
+def remove_duplicates(starting_list: list) -> list:
+    new_list: list = []
+    for i in starting_list:
+        n = starting_list.count(i)
+        if n == 1:
+            new_list.append(i)
+        elif n >= 2 and i not in new_list:
+            new_list.append(i)
+
+    return new_list
+
 
 print(remove_duplicates([1, 2, 3, 1, 2, 4])) #--> [1, 2, 3, 4]
 print(remove_duplicates([4, 5, 'a', 4, 6])) #--> [4, 5, 'a', 6]
+
+#ESERCIZIO 8
+#Scrivere la funzione chiamata seconds_since_noon che riceva il tempo espresso come tre argomenti interi (ore, minuti e secondi)
+# e restituisca il numero dei secondi da quando l'orologio "ha battuto le 12" l'ultima volta 
+#(le ore 12, dunque, vengono considerate come orario di partenza, dunque, come uno zero).
+
+#Ad esempio, alle ore 3:15:50 sono passate 3 ore, 15 minuti e 50 secondi, ovvero sono passati 11750 secondi 
+#da quando l'orologio ha "battuto le 12" per l'ultima volta.
+
+#Definire, poi, la funzione chiamata time_difference che prende come argomento due orari, 
+#entrambi espressi mediante ore, minuti e secondi. 
+#La funzione time_difference deve usare la funzione seconds_since_noon per calcolare la quantità di tempo in secondi tra due orari, 
+#entrambi contenuti entro un ciclo dell'orologio di 12 ore.
+
+#Ad esempio, tra le ore 1:00 e 3:15:30 sono passati 8130 secondi.
+
