51abe20e63016cfb3e2c54e4abe765863d13a9fb
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-22 16:37:32 +0000

    .

--- lezione10/lezione10.py
+++ lezione10/lezione10.py
@@ -140,6 +140,7 @@ def remove_duplicates(starting_list: list) -> list:
 print(remove_duplicates([1, 2, 3, 1, 2, 4])) #--> [1, 2, 3, 4]
 print(remove_duplicates([4, 5, 'a', 4, 6])) #--> [4, 5, 'a', 6]
 
+
 #ESERCIZIO 8
 #Scrivere la funzione chiamata seconds_since_noon che riceva il tempo espresso come tre argomenti interi (ore, minuti e secondi)
 # e restituisca il numero dei secondi da quando l'orologio "ha battuto le 12" l'ultima volta 
@@ -154,4 +155,43 @@ print(remove_duplicates([4, 5, 'a', 4, 6])) #--> [4, 5, 'a', 6]
 #entrambi contenuti entro un ciclo dell'orologio di 12 ore.
 
 #Ad esempio, tra le ore 1:00 e 3:15:30 sono passati 8130 secondi.
+
+def seconds_since_noon(ore:int, minuti:int, secondi:int):
+    pass
+
+def time_difference(orari: tuple):
+    pass
+
+
+print(time_difference(1, 0, 0, 3, 15, 30)) # --> 8130
+print(time_difference(0, 0, 0, 12, 0, 0)) # --> 43200
+
+#ESERCIZIO 9
+#Si scriva una funzione in Python che simuli una palla che rimbalza calcolando la sua altezza da terra in centimetri 
+#per ogni secondo, a mano a mano che il tempo passa su un orologio simulato.
+
+#Al tempo zero la palla comincia ad altezza zero e ha una velocità iniziale di 100 cm/s.
+
+#Dopo ogni secondo, il valore dell'altezza cambia, aggiungendo al valore corrente dell'altezza il valore della velocità corrente; 
+#poi, il valore della velocità viene modificato, sottraendo 96 al valore della velocità corrente.
+#Dunque, dopo ogni secondo, si ha che
+#altezza = altezza + velocità
+#velocità = velocità - 96.
+
+#Se il nuovo valore che si ottiene per l'altezza è inferiore a 0, si deve moltiplicare altezza e velocità per -0.5 
+#per simulare il rimbalzo. Dunque, per il rimbalzo, si avrà che
+#altezza= altezza*-0,5 
+#velocità=velocità*-0,5.
+
+#Ci si fermi al quinto rimbalzo.
+ 
+#Per ogni secondo, la funzione deve stampare il tempo trascorso e l'altezza a cui si trova la palla in quel determinato secondo.
+#Ad esempio, se al tempo 0, la palla si trova ad altezza 0 cm, allora la funzione stamperà:
+ 
+#"Tempo: 0 Altezza: 0"
+
+#Se avviene il rimbalzo, la funzione deve stampare il tempo trascorso e la parola "Rimbalzo!".
+#Ad esempio, se il rimbalzo avviene al tempo 4, allora la funzione stamperà:
+ 
+#"Tempo: 4 Rimbalzo!"
 
