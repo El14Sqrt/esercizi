021c171b9ad656bd7698b37f3fd0e9add16b6483
El14Sqrt <167420791+El14Sqrt@users.noreply.github.com>
2024-05-24 17:55:30 +0000

    .

--- lezione8/lezione8bis.py
+++ lezione8/lezione8bis.py
@@ -7,11 +7,20 @@
 #Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
 
 def longest_palindrome(s: str) -> int:
-    s = s.lower()
-    s_rev = s[::-1]
-    palindrome_string: list = []
-    all_palindrome: list = []
-            
+    length = 0
+    odd_found = False
+    
+
+    for char in set(s):  
+        count = s.count(char)
+        length += count // 2 * 2  
+        if count % 2 == 1:
+            odd_found = True
+    
+    if odd_found:
+        length += 1
+    
+    return length
 
 
 print(longest_palindrome("abccccdd"))
@@ -24,20 +33,64 @@ print(longest_palindrome("Aa"))
 
 #Implement the MyStack class:
 
-#- push(x: int) -> None: Pushes element x to the top of the stack.
+#- Push(x: int) -> None: Pushes element x to the top of the stack.
 #- Top() -> None: Removes the element on the top of the stack and returns it.
-#- pop() -> None: Returns the element on the top of the stack.
-#- empty() -> None: Returns true if the stack is empty, false otherwise.
+#- Pop() -> None: Returns the element on the top of the stack.
+#- Empty() -> None: Returns true if the stack is empty, false otherwise.
 
 class Queue:
-    pass
+    def __init__(self) -> None:
+        self.items = []
 
-
+    def in_queue(self, item):
+        if item not in self.items:
+            self.items.append(item)
+    
+    def out_queue(self, item):
+        if item in self.items:
+            return self.items.pop(0)
+    
+    def is_empty(self):
+        if len(self.items) == 0:
+            return "empty queue"
+         
 class MyStack:
-    pass
+    def __init__(self) -> None:
+        self.queue1 = Queue()
+        self.queue2 = Queue()
+
+    def Push(self, x: int) -> None:
+        self.queue1.in_queue(x)
+
+    def Top(self) -> None:
+        while len(self.queue1.items) > 1:
+            self.queue2.in_queue(self.queue1.out_queue())
+        top_element = self.queue1.out_queue()
+        
+        self.queue2.in_queue(top_element)
+        
+        self.queue1, self.queue2 = self.queue2, self.queue1
+        
+        return top_element
+    
+    
+    def Pop(self) -> None:
+        while len(self.queue1.items) > 1:
+            self.queue2.in_queue(self.queue1.out_queue())
+        
+        pop_element = self.queue1.out_queue()
+        
+        self.queue1, self.queue2 = self.queue2, self.queue1
+        
+        return pop_element
+    
+
+    def Empty(self) -> None:
+        return self.queue1.is_empty() and self.queue2.is_empty()
 
 
 
+    
 #ESERCIZIO 5
 
 #Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', write a function to determine if the input string is valid.
@@ -79,7 +132,21 @@ print(is_valid_parenthesis("()[]{}"))
 #and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
 def merge(nums1, m, nums2, n):
-    pass
+    p1,p2 =m-1,n-1
+    p=m+n-1
+    while p1 >=0 and p2>=0:
+        if nums1[p1]>nums2[p2]:
+            nums1[p] = nums1[p1]
+            p1-=1
+        else:
+            nums1[p]=nums2[p2]
+            p2 -=1
+        p -=1
+    while p2 >=0:
+        nums1[p] = nums2[p2]
+        p2-=1
+        p-=1
+    return nums1
 
 nums1 = [1, 2, 3, 0, 0, 0]
 m = 3
