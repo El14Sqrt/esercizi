8b8ad4ffbe1d1ce40482cf7fef958255590a94e2
El14Sqrt <4r0.squarti@gmail.com>
2024-05-31 13:57:43 +0200

    -

--- lezione12/lezione12.py
+++ lezione12/lezione12.py
@@ -1,3 +1,14 @@
 i: int = 0
+test_value:int = 0
 
-assert i == 0
+assert i == test_value, f"error, the variable i must be {test_value}, instead is {i}"
+
+def sum(a:int, b:int) -> int:
+    return 0
+
+result = sum(a=5, b=4)
+test_value: int = 9
+assert result == test_value, f"error, the variable i must be {test_value}, instead is {result}"
+
+#all()
+
