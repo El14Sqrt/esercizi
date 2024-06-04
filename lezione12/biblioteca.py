014eff6c402c67f08ccfd60d1bda8b50e0b5c914
El14Sqrt <4r0.squarti@gmail.com>
2024-06-04 11:51:59 +0200

    file creato

---
+++ lezione12/biblioteca.py
@@ -0,0 +1,23 @@
+class Libro:
+    def __init__(self, titolo:str, autore:str, stato) -> None:
+        self.titolo=titolo
+        self.autore=autore
+        self.stato=stato
+
+class Bibioteca:
+    def __init__(self) -> None:
+        pass
+
+    def aggiungi_libro(self, libro:Libro):
+        pass
+
+    def presta_libro(self):
+        pass
+
+    def restituisci_libro(self):
+        pass
+
+    def mostra_libri(self):
+        pass
+
+    
---
+++ lezione12/catalogo_film.py
@@ -0,0 +1,21 @@
+class MovieCatalog:
+    def __init__(self) -> None:
+        pass
+
+    def add_movie(self, director_name:str, movies):
+        pass
+
+    def remove_movie(self, director_name:str, movies):
+        pass
+
+    def list_directors(self):
+        directors = list()
+        pass
+
+    def get_movies_by_director(self, director_name:str):
+        pass
+
+    def get_movies_by_title(self, title:str):
+        pass
+
+    
