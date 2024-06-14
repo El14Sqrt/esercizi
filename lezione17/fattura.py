2a16e1e2cdc52921623cbfb763045a5a455755e8
El14Sqrt <4r0.squarti@gmail.com>
2024-06-14 13:51:03 +0200

    lez 17

---
+++ lezione17/fattura.py
@@ -0,0 +1,19 @@
+from ...esercizi.lezione17.paziente import Paziente
+from ...esercizi.lezione17.dottore import Dottore
+
+class Fattura:
+    def __init__(self) -> None:
+        pass
+
+    def getSalary(self):
+        pass
+
+    def getFatture(self):
+        pass
+
+    def addPatient(self, newPatient):
+        pass
+
+    def removePatient(self, idCode):
+        pass
+
