4d5fcc944a5b9d2f2befafce6926081dca1e0bc6
El14Sqrt <4r0.squarti@gmail.com>
2024-06-18 13:57:33 +0200

    .

---
+++ lezione19/merge.py
@@ -0,0 +1,8 @@
+import random
+lunghezza_array = random.randint(10, 30)
+array: list = [random.randint(0, 100) for _ in range(lunghezza_array)]
+print(array)
+
+mid_point = len(array) // 2
+print(len(array))
+
