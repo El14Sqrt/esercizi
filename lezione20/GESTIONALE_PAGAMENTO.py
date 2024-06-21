a595770fe49bf694084f9f159e6e9c7564f25a7b
El14Sqrt <4r0.squarti@gmail.com>
2024-06-21 19:15:07 +0200

    -

--- lezione20/GESTIONALE_PAGAMENTO.py
+++ lezione20/GESTIONALE_PAGAMENTO.py
@@ -1,2 +1,23 @@
 class Pagamento:
-    
+    def __init__(self) -> None:
+        pass
+
+    def set(self):
+        pass
+    def get(self):
+        pass
+
+    def dettagliPagamento(self):
+        pass
+
+
+    class PagamentoContanti(Pagamento):
+        pass
+
+        def inPezziDa(self):
+            pass
+        
+
+    class PagamentoCartaDiCredito(Pagamento):
+        pass
+
